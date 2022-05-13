56233507fe29a0f4402bce3652b12fa33144a925
Alessandro Murtas <alessandro.murtas@galileo.galileicrema.it>
2022-05-13 20:10:58 +0200

    Saluto iniziale
    Aggiunta la stampa a console che saluta l'utente all'avvio del programma

--- Prova/src/prova/Prova.java
+++ Prova/src/prova/Prova.java
@@ -13,8 +13,9 @@ public class Prova {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    public static void main(String[] args)
+    {
+        System.out.println("Prova!");
     }
     
 }
